bdb5a03a6b9fdcf6a3616a88d67785bead1276c1
joseorhoseb <bengerould@gmail.com>
2012-11-13 17:23:49 -0600

    Update np.pl
    manual update with website

--- np.pl
+++ np.pl
@@ -1,13 +1,24 @@
 #!/usr/bin/perl
 while(<>) {
-  my ($first, $last) = /(\S+)\s+(\S+)/;
-
-  #name with period as enter
+  my ($first,$last) = /(\S+)\s+(\S+)/;
+  #John.Smith/john.smith/JOHN.SMITH
   print "$first.$last\n";
-  #name with period tolower()
   print lc "$first.$last\n";
-  #name with period toupper()
   print uc "$first.$last\n";
-
-  print substr($first, 0, 1) . $last;
+  #JSmith/jsmith/JSMITH
+  print substr($first,0,1).$last;print"\n";
+  print lc substr($first,0,1).$last;print"\n";
+  print uc substr($first,0,1).$last;print"\n";
+  #JohnS/johns/JOHNS
+  print $first.substr($last,0,1);print"\n";
+  print lc substr($first,0,1).$last; print"\n";
+  print uc substr($first,0,1).$last;print"\n";
+  #SmithJ/smithj/SMITHJ
+  print $last.substr($first,0,1);print"\n";
+  print lc $last.substr($first,0,1);print"\n";
+  print uc $last.substr($first,0,1);print"\n";
+  #SJohn/sjohn/SJOHN
+  print substr($last,0,1).$first;print"\n";
+  print lc substr($last,0,1).$first;print"\n";
+  print uc substr($last,0,1).$first;print"\n";
 }
