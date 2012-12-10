289237ad0d95f5dc20215573d68bacfb347d1e13
James Koval <james.ross.koval@gmail.com>
2012-12-09 19:08:18 -0800

    deduplicate

--- np.pl
+++ np.pl
@@ -1,24 +1,39 @@
 #!/usr/bin/perl
+use strict;
+use warnings;
+
 while(<>) {
-  my ($first,$last) = /(\S+)\s+(\S+)/;
-  #John.Smith/john.smith/JOHN.SMITH
-  print "$first.$last\n";
-  print lc "$first.$last\n";
-  print uc "$first.$last\n";
-  #JSmith/jsmith/JSMITH
-  print substr($first,0,1).$last;print"\n";
-  print lc substr($first,0,1).$last;print"\n";
-  print uc substr($first,0,1).$last;print"\n";
-  #JohnS/johns/JOHNS
-  print $first.substr($last,0,1);print"\n";
-  print lc substr($first,0,1).$last; print"\n";
-  print uc substr($first,0,1).$last;print"\n";
-  #SmithJ/smithj/SMITHJ
-  print $last.substr($first,0,1);print"\n";
-  print lc $last.substr($first,0,1);print"\n";
-  print uc $last.substr($first,0,1);print"\n";
-  #SJohn/sjohn/SJOHN
-  print substr($last,0,1).$first;print"\n";
-  print lc substr($last,0,1).$first;print"\n";
-  print uc substr($last,0,1).$first;print"\n";
+  my %permutations;
+
+  if (/^(\S+)\s+(\S+)$/) { # First and Last name, two words
+    #John.Smith/john.smith/JOHN.SMITH
+    $permutations{"$1.$2"} = 1;
+    $permutations{lc "$1.$2"} = 1;
+    $permutations{uc "$1.$2"} = 1;
+    #JSmith/jsmith/JSMITH
+    $permutations{substr($1,0,1)} = 1;
+    $permutations{lc substr($1,0,1).$2} = 1;
+    $permutations{uc substr($1,0,1).$2} = 1;
+    #JohnS/johns/JOHNS
+    $permutations{$1.substr($2,0,1)} = 1;
+    $permutations{lc substr($1,0,1).$2} = 1;
+    $permutations{uc substr($1,0,1).$2} = 1;
+    #SmithJ/smithj/SMITHJ
+    $permutations{$2.substr($1,0,1)} = 1;
+    $permutations{lc $2.substr($1,0,1)} = 1;
+    $permutations{uc $2.substr($1,0,1)} = 1;
+    #SJohn/sjohn/SJOHN
+    $permutations{substr($2,0,1).$1} = 1;
+    $permutations{lc substr($2,0,1).$1} = 1;
+    $permutations{uc substr($2,0,1).$1} = 1;
+  }
+
+  if (/^(\S+)\s+(\S+)\s+(\S+)$/) { # First, Middle and Last name, three words
+    #John Carl Smith
+    $permutations{"$1.$2.$3\n"} = 1;
+    $permutations{lc "$1.$2.$3\n"} = 1;
+    $permutations{uc "$1.$2.$3\n"} = 1;
+  }
+
+  print join "\n", keys %permutations;
 }
